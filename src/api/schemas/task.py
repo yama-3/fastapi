e5cf993a56aa76d88d6344445752fb25dafe7cf6
kazuhiro yamabe <kazuhiro.yamabe@gmail.com>
2022-12-06 14:26:59 +0000

    implement schema classes, mock routers

--- src/api/schemas/task.py
+++ src/api/schemas/task.py
@@ -1,8 +1,25 @@
 from typing import Optional
-
 from pydantic import BaseModel, Field
 
-class Task(BaseModel):
+
+class TaskBase(BaseModel):
+    title: Optional[str] = Field(None, example="クリーニングを撮りに行く")
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskCreateResponse(TaskCreate):
     id: int
-    title: Optional[str] = Field(None, example="クリ＝ニングを取りに行く")
+
+    class Config:
+        orm_mode = True
+
+
+class Task(TaskBase):
+    id: int
     done: bool = Field(False, description="完了フラグ")
+
+    class Config:
+        orm_mode = True
