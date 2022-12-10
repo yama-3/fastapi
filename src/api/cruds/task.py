e90d5879050c2db37f57e98e9efb87b54e8b45f7
kazuhiro yamabe <kazuhiro.yamabe@gmail.com>
2022-12-10 18:11:59 +0900

    implement: Delete

--- src/api/cruds/task.py
+++ src/api/cruds/task.py
@@ -38,3 +38,7 @@ async def update_task(db: AsyncSession, task_create: task_schema.TaskCreate, ori
     await db.commit()
     await db.refresh(original)
     return original
+
+async def delete_task(db: AsyncSession, original: task_model.Task) -> None:
+    await db.delete(original)
+    await db.commit()
